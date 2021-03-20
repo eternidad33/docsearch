772d6509be71e6b582c379dda99d791f0f6019e5
vigilr <3496655347@qq.com>
2021-03-20 11:55:42 +0800

    neo4j 创建节点

---
+++ dataProcess/entity/article.py
@@ -0,0 +1,13 @@
+class Article:
+    def __init__(self, url='', title='', authors=[], summary='', keys=[], funds=[], doi='', album='', special='',
+                 classNo=''):
+        self.url = url
+        self.title = title
+        self.authors = authors
+        self.summary = summary
+        self.keys = keys
+        self.funds = funds
+        self.doi = doi
+        self.album = album
+        self.special = special
+        self.classNo = classNo
---
+++ dataProcess/readData/readArticle.py
@@ -0,0 +1,12 @@
+import csv
+
+f = open('../../crawl/zhiwang/csv/article.csv', encoding='utf-8')
+reader = csv.reader(f)
+header_row = next(reader)
+for row in reader:
+    # row为每一行元素
+    if len(row) == 9:
+        # print(row)
+        name = row[0]
+        classNo = row[8]
+        print('title:{},classNo:{}'.format(name, classNo))
